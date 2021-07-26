3ee15221104e91b0d45e361cdc0c0f73d6a1d898
karishmas6 <carishmashukla28@gmail.com>
2021-07-26 15:31:42 +0530

    Setting up ESBuild

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        console.log(service);
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = () => {
+        console.log(input);
+    };
+
+    return <div>
+        <textarea 
+        value = {input}
+        onChange={e => setInput(e.target.value)}
+        />
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
