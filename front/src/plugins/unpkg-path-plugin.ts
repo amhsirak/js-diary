4e3d3bea854e3b54f9f9d3995a4c7281fffa7738
karishmas6 <carishmashukla28@gmail.com>
2021-07-27 12:57:02 +0530

    Attempting Dynamic Fetching

--- front/src/plugins/unpkg-path-plugin.ts
+++ front/src/plugins/unpkg-path-plugin.ts
@@ -1,31 +1,41 @@
-import * as esbuild from 'esbuild-wasm';
- 
+import * as esbuild from "esbuild-wasm";
+import axios from 'axios';
+
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       // onResolve: Find path where the module is stored
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if(args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
         }
       });
-      
+
       // onLoad: Attempts to load a file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
           return {
-            loader: 'jsx',
+            loader: "jsx",
             contents: `
               import message from 'tiny-test-pkg';
             `,
           };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
