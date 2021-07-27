6fa55e7e3acff81157d2a0f1c355890e764bc5ea
karishmas6 <carishmashukla28@gmail.com>
2021-07-27 12:47:31 +0530

    Attempting Dynamic Fetching

--- front/src/plugins/unpkg-path-plugin.ts
+++ front/src/plugins/unpkg-path-plugin.ts
@@ -4,11 +4,17 @@ export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // onResolve: Find path where the module is stored
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log('onResolve', args);
+        if(args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg') {
+          return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
+        }
       });
- 
+      
+      // onLoad: Attempts to load a file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
  
@@ -16,14 +22,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
-              console.log(message);
+              import message from 'tiny-test-pkg';
             `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
           };
         }
       });
