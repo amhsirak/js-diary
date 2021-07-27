a13afeca23425f48e2813997da7e6ce14c6c9ec4
karishmas6 <carishmashukla28@gmail.com>
2021-07-27 15:14:21 +0530

    Dynamic Fetching

--- front/src/plugins/unpkg-path-plugin.ts
+++ front/src/plugins/unpkg-path-plugin.ts
@@ -33,15 +33,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('nested-test-pkg');
+              console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        // console.log(request);
         return {
           loader: "jsx",
           contents: data,
+          // where we found the nested package
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
