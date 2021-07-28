464ddee75d635753f7d85df8d2bc43f51ba5f7a5
karishmas6 <carishmashukla28@gmail.com>
2021-07-28 15:20:22 +0530

    Regex in plugin

--- front/src/plugins/unpkg-path-plugin.ts
+++ front/src/plugins/unpkg-path-plugin.ts
@@ -10,27 +10,28 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      // onResolve: Find path where the module is stored
+
+      // Regex : if(args.path === index.js) 
+      build.onResolve({ filter: /(^index\.js$)/}, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+      
+      // Regex : if (args.path.includes('./') || args.path.includes('../')) find relative paths
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href,
+        }
+      });
+
+      // Regex: find main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href,
-          }
-        }
-
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
       });
 
-      // onLoad: Attempts to load a file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
 
@@ -62,3 +63,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
     },
   };
 };
+
+// onResolve: Find path where the module is stored
+ // onLoad: Attempts to load a file
