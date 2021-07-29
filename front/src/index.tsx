ededddd05c6fd5dd7512822e3cbdee6bafffc465
karishmas6 <carishmashukla28@gmail.com>
2021-07-29 12:35:35 +0530

    Code Cleaning

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -12,7 +12,7 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
 
     };
