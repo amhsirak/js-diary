6d2dead08b7a489e1c527622e79b4f1a6c7329d8
karishmas6 <carishmashukla28@gmail.com>
2021-07-30 15:33:14 +0530

    displaying error

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -52,7 +52,13 @@ const App = () => {
             <div id="root"></div>
             <script>
                 window.addEventListener('message', (event) => {
-                    eval(event.data);
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                        console.error(err);
+                    }
                 }, false);
             </script>
         </body>
