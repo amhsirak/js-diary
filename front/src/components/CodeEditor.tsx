27b603f417afb3a97a2a75065f90b33e6f6196b5
karishmas6 <carishmashukla28@gmail.com>
2021-07-31 15:50:29 +0530

    Code Editor

--- front/src/components/CodeEditor.tsx
+++ front/src/components/CodeEditor.tsx
@@ -6,7 +6,14 @@ const CodeEditor = () => {
     height="500px"
     theme="dark" 
     options={{
-        wordWrap: "on"
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 18,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
     }}
     />;
 }
