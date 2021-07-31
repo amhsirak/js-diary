3d7f11b0e6221ecde9f424523659b94b9c28db61
karishmas6 <carishmashukla28@gmail.com>
2021-07-31 15:44:07 +0530

    Code Editor

--- front/src/components/CodeEditor.tsx
+++ front/src/components/CodeEditor.tsx
@@ -1,7 +1,14 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <MonacoEditor height="500px" />;
+    return <MonacoEditor
+    language="javascript"
+    height="500px"
+    theme="dark" 
+    options={{
+        wordWrap: "on"
+    }}
+    />;
 }
 
 export default CodeEditor;
