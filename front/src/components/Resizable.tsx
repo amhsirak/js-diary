5af472fbc48eea7dd646f2ce503a1966a7d7e534
karishmas6 <carishmashukla28@gmail.com>
2021-08-03 10:43:37 +0530

    Resizing cell

--- front/src/components/Resizable.tsx
+++ front/src/components/Resizable.tsx
@@ -1,5 +1,6 @@
 import "../styles/Resizable.css";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
+import { useEffect } from "react";
 
 interface ResizableProps {
     direction: "horizontal" | "vertical";
@@ -8,6 +9,17 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
     let resizableProps: ResizableBoxProps;
+
+    useEffect(() => {
+        const listener = () => {
+            console.log(window.innerWidth,window.innerHeight);
+        }
+        window.addEventListener("resize", listener);
+        return () => {
+            window.removeEventListener("resize", listener);
+        }
+    },[])
+
     if (direction === "horizontal") {
         resizableProps = {
             className: "resize-horizontal",
