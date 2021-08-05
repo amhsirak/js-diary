f81d5b00fd5d55d7c45ff19367cf3784e30476b7
karishmas6 <carishmashukla28@gmail.com>
2021-08-05 23:16:10 +0530

    Text Editor Styling

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -3,40 +3,47 @@ import "../styles/TextEditor.css";
 import { useState, useRef, useEffect } from "react";
 
 const TextEditor: React.FC = () => {
-    const [editing, setEditing] = useState(false);
-    const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
+  const ref = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        const listener = (event:MouseEvent) => {
-            if (ref.current && event.target && ref.current.contains(event.target as Node)) {
-                console.log("Element inside editor");
-                return;
-            }
-            setEditing(false);
-        }
-        document.addEventListener("click", listener, {
-            capture: true
-        });
-        return () => {
-            document.removeEventListener("click", listener, {
-                capture: true
-            });
-        }
-    }, []);
-
-    if (editing) {
-        return (
-            <div className="text-editor" ref={ref}>
-                <MDEditor />
-            </div>
-        );
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log("Element inside editor");
+        return;
+      }
+      setEditing(false);
     };
+    document.addEventListener("click", listener, {
+      capture: true,
+    });
+    return () => {
+      document.removeEventListener("click", listener, {
+        capture: true,
+      });
+    };
+  }, []);
 
+  if (editing) {
     return (
-        <div className="text-editor" onClick={() => setEditing(true)} >
-            <MDEditor.Markdown source={"# Header"} />
-        </div>
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
+      </div>
     );
+  }
+
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
 };
 
 export default TextEditor;
