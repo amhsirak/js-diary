c49aae552900e0f71c9cc13f04a3945d4c0674f5
karishmas6 <carishmashukla28@gmail.com>
2021-08-09 14:25:09 +0530

    Cell rendering

--- src/components/CellListItem.tsx
+++ src/components/CellListItem.tsx
@@ -1,12 +1,20 @@
 import { Cell } from "../state";
+import CodeCell from "./CodeCell";
+import TextEditor from "./TextEditor";
 
 interface CellListItemProps {
     cell: Cell
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+    let child:JSX.Element; 
+    if (cell.type === "code") {
+        child = <CodeCell />
+    } else {
+        child = <TextEditor />
+    }
     return <div>
-        {cell.id}
+        {child}
     </div>
 };
 export default CellListItem;
