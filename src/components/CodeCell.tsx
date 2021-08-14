416b5e85c16244d064d9713f4d283390984d24c5
karishmas6 <carishmashukla28@gmail.com>
2021-08-14 18:29:09 +0530

    UI fixes

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -18,15 +18,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   // console.log(bundle);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async() => {
        // Code transpiling and bundling
-       createBundle(cell.id, cell.content)
+       createBundle(cell.id, cell.content);
     },800);
 
     return () => {
       clearTimeout(timer);
     }
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[cell.content, cell.id, createBundle]);
 
   return (
@@ -38,7 +42,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        { bundle && <Preview code={bundle.code} err={bundle.err} /> }
+        <div className="progress-wrapper">
+        {
+          !bundle || bundle.loading ? (
+           <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">
+              Loading
+            </progress>
+          </div>
+          ) : ( 
+            <Preview code={bundle.code} err={bundle.err} /> 
+            )}
+        </div>
       </div>
     </Resizable>
   );
