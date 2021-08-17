bb756fdb5c408c57d87340d3014a30b39dc152ef
karishmas6 <carishmashukla28@gmail.com>
2021-08-17 11:36:25 +0530

    Display function

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -18,26 +18,31 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-      const display = (value) => {
-        const root = document.querySelector("#root");
-        if (typeof value === "object") {
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, root);
-          } else {
-          root.innerHTML = JSON.stringify(value);
-          } 
+    const displayFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    var display = (value) => {
+      const root = document.querySelector("#root");
+      if (typeof value === "object") {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
         } else {
-         root.innerHTML = value;
-        }
+        root.innerHTML = JSON.stringify(value);
+        } 
+      } else {
+       root.innerHTML = value;
       }
-      `
-    ];
+    }
+    `;
+    const displayFuncNoOp = 'var display = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          cumulativeCode.push(displayFunc);
+        } else {
+          cumulativeCode.push(displayFuncNoOp);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
