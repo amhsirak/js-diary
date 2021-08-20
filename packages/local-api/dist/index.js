7620da8da49122f523ef230528a15977e50aa980
karishmas6 <carishmashukla28@gmail.com>
2021-08-20 17:07:56 +0530

    Local API

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,8 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var serve = function (port, filename, dir) {
     var app = express_1.default();
+    app.use(http_proxy_middleware_1.createProxyMiddleware({
+        target: "http://localhost:3000",
+        ws: true,
+        logLevel: "silent"
+    }));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on("error", reject);
     });
