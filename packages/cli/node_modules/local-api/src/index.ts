8368f7bddecc22e24e74244104ff0f4b28ba3d5b
karishmas6 <carishmashukla28@gmail.com>
2021-08-23 20:32:51 +0530

    basic proxy handling

--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
 
-export const serve = (port: number, filename: string, dir: string) => {
-    const app = express();
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app = express();
 
+  if (useProxy) {
+    // for CRA development server
     app.use(createProxyMiddleware({
-        target: "http://localhost:3000",
-        ws: true,
-        logLevel: "silent"
-    }));
+         target: "http://localhost:3000",
+         ws: true,
+         logLevel: "silent"
+     }));
+  } else {
+      // for local machine
+    const packagePath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
-    return new Promise<void>((resolve,reject) => {
-        app.listen(port, resolve).on("error", reject);
-    });
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
