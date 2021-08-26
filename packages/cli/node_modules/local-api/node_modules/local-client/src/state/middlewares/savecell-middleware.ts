0c721ab7b6c26d636e2e2bb12c7569231e4581e0
karishmas6 <carishmashukla28@gmail.com>
2021-08-26 20:41:09 +0530

    Save cells middleware

--- packages/cli/node_modules/local-api/node_modules/local-client/src/state/middlewares/savecell-middleware.ts
+++ packages/cli/node_modules/local-api/node_modules/local-client/src/state/middlewares/savecell-middleware.ts
@@ -1,14 +1,33 @@
-import { Dispatch } from "redux"
-import { Action } from "../actions"
-import { ActionType } from "../action-types"
-import { saveCellsError } from "../action-creators";
+import { Dispatch } from "redux";
+import { Action } from "../actions";
+import { ActionType } from "../action-types";
+import { saveCells } from "../action-creators";
+import { RootState } from "../reducers";
 
-export const saveCellMiddleware = ({ dispatch }: { dispatch: Dispatch<Action>}) => {
-    return (next: (action: Action) => void) => {
-        return (action: Action) => {
-            next(action);
-        };
+export const saveCellMiddleware = ({
+  dispatch,
+  getState,
+}: {
+  dispatch: Dispatch<Action>;
+  getState: () => RootState;
+}) => {
+  return (next: (action: Action) => void) => {
+    return (action: Action) => {
+      next(action);
+
+      if (
+        [
+          ActionType.MOVE_CELL,
+          ActionType.DELETE_CELL,
+          ActionType.INSERT_CELL_AFTER,
+          ActionType.UPDATE_CELL,
+        ].includes(action.type)
+      ) {
+        // because saveCells() uses redux thunk
+        saveCells()(dispatch, getState);
+      }
     };
+  };
 };
 
 // use of this middleware: Want to dispatch an additional action after recieving all the actions
